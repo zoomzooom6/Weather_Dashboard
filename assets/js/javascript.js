7d2884326eeafb5e2e70417867b5649ba63b9a82
zoomzoom6 <tonyhuang107@live.ca>
2021-06-01 15:11:30 -0400

    enable clicking search history to search again

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -35,84 +35,89 @@ var fiveTemp = document.getElementById('tempForeFive');
 var fiveWind = document.getElementById('windForeFive');
 var fiveHum = document.getElementById('humForeFive');
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
     var searchCity = citySearch.value;
 
     if (searchCity) {
-        currentCity(searchCity);
+        currentCity(searchCity, 0);
         citySearch.value = "";
-    } 
+    }
 };
 
-var historySearch = function(event) {
-    event.preventDefault();
+var historySearch = function (event) {
+    currentCity(event.target.id, 1);
 
-    var clickedBtn = $(".btn").attr("id");
-    console.log(clickedBtn);
-    
 }
 
-var searchHistory = function(city) {
-    var buttonE1 = document.createElement('button');
+var searchHistory = function (city) {
+    var buttonE1 = document.createElement('p');
     buttonE1.classList = "btn padded";
     buttonE1.textContent = city;
-    buttonE1.setAttribute('type', 'submit');
+    //buttonE1.setAttribute('type', 'submit');
     buttonE1.setAttribute('id', city);
     searchHist.appendChild(buttonE1);
 };
 
-var currentCity = function(location) {
+var currentCity = function (location, add) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=9f2a573047350fc85e320b551e5f6ee3&units=metric"
 
     fetch(apiUrl)
-    .then(function(response) {
-        if(response.ok) {
-            response.json().then(function (data) {
-                var lat = data.coord.lat;
-                var lon = data.coord.lon;
-                currentTemp(lat, lon, data.name);
-                searchHistory(location);
-            })
-        } else {
-            alert("Unable to find city, please check your spelling.");
-        }
-    });
+        .then(function (response) {
+            if (response.ok) {
+                if (add === 0) {
+                    response.json().then(function (data) {
+                        var lat = data.coord.lat;
+                        var lon = data.coord.lon;
+                        currentTemp(lat, lon, data.name);
+                        searchHistory(location);
+                    })
+                } else {
+                    response.json().then(function (data) {
+                        var lat = data.coord.lat;
+                        var lon = data.coord.lon;
+                        currentTemp(lat, lon, data.name);
+                    })
+                }
+            } else {
+                alert("Unable to find city, please check your spelling.");
+            }
+        });
 };
 
-var currentTemp = function(lat, lon, city) {
+var currentTemp = function (lat, lon, city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=9f2a573047350fc85e320b551e5f6ee3&units=metric"
 
     fetch(apiUrl)
-    .then(function(response) {
-        if(response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-                weatherDisp(data, city);
-            })
-        }
-    });
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    weatherDisp(data, city);
+                })
+            }
+        });
 };
 
-var fiveDayforecast = function(location) {
+var fiveDayforecast = function (location) {
     setDate();
     setFutureTemp(location);
     setFutureWind(location);
     setFutureHumidity(location);
 };
 
-var weatherDisp = function(info, city) {
+var weatherDisp = function (info, city) {
     var date = moment().format("M/D/YYYY")
     var cityName = city;
     var iconE1 = document.createElement('img');
     iconE1.classList = "icon";
-    
+
     //get current weather icon from API response
     iconE1.setAttribute("src", weatherIcon = "http://openweathermap.org/img/wn/" + info.current.weather[0].icon + ".png");
     cityLocation.textContent = cityName + " (" + date + ") ";
     cityLocation.appendChild(iconE1);
-    
+
     //set Temperature, Wind, Humidity, and UV for chosen city
     cityTemp.textContent = "Temp: " + info.current.temp + "°C" + " (Feels like: " + info.current.feels_like + "°C)";
     cityWind.textContent = "Wind: " + info.current.wind_speed + " km/h";
@@ -122,11 +127,11 @@ var weatherDisp = function(info, city) {
     uvRating.innerHTML = info.current.uvi;
 
     //change UV rating background based on value
-    if(info.current.uvi < 2) {
+    if (info.current.uvi < 2) {
         uvRating.classList = "rating uvLow";
     } else if (info.current.uvi < 5) {
         uvRating.classList = "rating uvMod";
-    } else if (info.current.uvi < 7 ) {
+    } else if (info.current.uvi < 7) {
         uvRating.classList = "rating uvHigh";
     } else {
         uvRating.classList = "rating uvDanger";
@@ -139,7 +144,7 @@ var weatherDisp = function(info, city) {
 };
 
 //function to label next 5 days
-var setDate = function() {
+var setDate = function () {
     oneDate.textContent = moment().add(1, 'd').format("M/D/YYYY");
     twoDate.textContent = moment().add(2, 'd').format("M/D/YYYY");
     threeDate.textContent = moment().add(3, 'd').format("M/D/YYYY");
@@ -148,7 +153,7 @@ var setDate = function() {
 };
 
 //function to show 5 day temperature forecast and icons
-var setFutureTemp = function(temp) {
+var setFutureTemp = function (temp) {
     oneTemp.textContent = "Temp: " + temp.daily[0].temp.day + "°C";
     twoTemp.textContent = "Temp: " + temp.daily[1].temp.day + "°C";
     threeTemp.textContent = "Temp: " + temp.daily[2].temp.day + "°C";
@@ -165,17 +170,17 @@ var setFutureTemp = function(temp) {
     iconE2.classList = "icon";
     iconE2.setAttribute("src", weatherIcon = "http://openweathermap.org/img/wn/" + temp.daily[1].weather[0].icon + ".png");
     twoDate.appendChild(iconE2);
-    
+
     var iconE3 = document.createElement('img');
     iconE3.classList = "icon";
     iconE3.setAttribute("src", weatherIcon = "http://openweathermap.org/img/wn/" + temp.daily[2].weather[0].icon + ".png");
     threeDate.appendChild(iconE3);
-    
+
     var iconE4 = document.createElement('img');
     iconE4.classList = "icon";
     iconE4.setAttribute("src", weatherIcon = "http://openweathermap.org/img/wn/" + temp.daily[3].weather[0].icon + ".png");
     fourDate.appendChild(iconE4);
-    
+
     var iconE5 = document.createElement('img');
     iconE5.classList = "icon";
     iconE5.setAttribute("src", weatherIcon = "http://openweathermap.org/img/wn/" + temp.daily[4].weather[0].icon + ".png");
@@ -183,7 +188,7 @@ var setFutureTemp = function(temp) {
 };
 
 //function to show 5 day wind forecast
-var setFutureWind = function(wind) {
+var setFutureWind = function (wind) {
     oneWind.textContent = "Wind: " + wind.daily[0].wind_speed + " km/h";
     twoWind.textContent = "Wind: " + wind.daily[1].wind_speed + " km/h";
     threeWind.textContent = "Wind: " + wind.daily[2].wind_speed + " km/h";
@@ -192,7 +197,7 @@ var setFutureWind = function(wind) {
 };
 
 //function to show 5 day humidity forecast
-var setFutureHumidity = function(humidity) {
+var setFutureHumidity = function (humidity) {
     oneHum.textContent = "Humidity: " + humidity.daily[0].humidity + "%";
     twoHum.textContent = "Humidity: " + humidity.daily[1].humidity + "%";
     threeHum.textContent = "Humidity: " + humidity.daily[2].humidity + "%";
@@ -201,6 +206,6 @@ var setFutureHumidity = function(humidity) {
 };
 
 userFormE1.addEventListener("submit", formSubmitHandler);
-searchHist.addEventListener("submit", historySearch);
+searchHist.addEventListener("click", historySearch);
 
 //forecastCall("Toronto");
